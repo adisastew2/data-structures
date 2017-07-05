f67e9387ba797ee76f56b03335c7f6b61e4d2fb7
Hack Reactor Student <student@hackreactor.com>
2017-07-05 16:06:35 -0700

    Implement prototypal stack/queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.length = 0;
+  obj.storage = {};
+
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.length] = value;
+    this.length++;
+  },
+  dequeue: function() {
+    if (this.length > 0) {
+      var value = this.storage[0];
 
+      this.length--;
+      for (var i = 0; i < this.length; i++) {
+        this.storage[i] = this.storage[i + 1];
+      }
 
+      return value;
+    }
+  },
+  size: function() {
+    return this.length;
+  }
+};
