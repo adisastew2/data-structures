2826db8d4f570cd002491dfa5ed39ffff4fca498
Hack Reactor Student <student@hackreactor.com>
2017-07-06 16:00:02 -0700

    Implement binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,53 @@
 var BinarySearchTree = function(value) {
+  var obj = Object.create(binarySearchTreeMethods);
+
+  obj.value = value;
+  obj.left = obj.right = null;
+
+  return obj;
 };
 
+binarySearchTreeMethods = {};
+
+binarySearchTreeMethods.insert = function(value) {
+  if (value < this.value) {
+    if (this.left) {
+      this.left.insert(value);
+    } else {
+      this.left = BinarySearchTree(value);
+    }
+  } else {
+    if (this.right) {
+      this.right.insert(value);
+    } else {
+      this.right = BinarySearchTree(value);
+    }
+  }
+};
+
+binarySearchTreeMethods.contains = function(value) {
+  if (this.value === value) {
+    return true;
+  } else if (value < this.value && this.left) {
+    return this.left.contains(value);
+  } else if (this.right) {
+    return this.right.contains(value);
+  }
+
+  return false;
+};
+
+binarySearchTreeMethods.depthFirstLog = function(func) {
+  func(this.value);
+
+  if (this.left) {
+    this.left.depthFirstLog(func);
+  }
+
+  if (this.right) {
+    this.right.depthFirstLog(func);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
