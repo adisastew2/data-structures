d9f3dcaea4cdb8baa0166eab0f0f713dae467096
Hack Reactor Student <student@hackreactor.com>
2017-07-07 15:20:58 -0700

    Refactor HashTable to use bucket arrays
    This is a complete redo, starting with pseudocode
    comments and filling in the gaps.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,66 +8,69 @@ var HashTable = function() {
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  // Get the current linked list head
-  var tuple = this._storage.get(index);
-  while (tuple) {
-    // if tuple exists, update its value and exit
-    if (tuple['key'] === k) {
-      tuple['value'] = v;
-      return;
-    }
-    // iterate through linked list
-    tuple = tuple['next'];
+  // Get the 'bucket' array from storage at index
+  var bucket = this._storage.get(index);
+  // if the 'bucket' array doesn't exist...
+  if (!bucket) {
+    // initialize and store 'bucket' in storage
+    bucket = [];
+    this._storage.set(index, bucket);
   }
 
-  // create a new tuple with key, value, and assign
-  // current linked list head to next.
-  tuple = [];
+  // Iterate through elements in 'bucket'
+  for (let i = 0; i < bucket.length; i++) {
+    var item = bucket[i];
+    // if array item key matches...
+    if (item['key'] === k) {
+      // update item value
+      item['value'] = v;
+      // and bail
+      return;
+    }
+  }
+
+  // create a new tuple with key and value
+  var tuple = [];
   tuple['key'] = k;
   tuple['value'] = v;
-  tuple['next'] = this._storage.get(index);
 
-  // set new tuple as current head
-  this._storage.set(index, tuple);
+  // push tuple onto bucket array
+  bucket.push(tuple);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  // get linked list head for index
-  var tuple = this._storage.get(index);
-  
-  while (tuple) {
-    // if tuple key matches, return value
-    if (tuple['key'] === k) {
-      return tuple['value'];
+  // Get the 'bucket' array from storage at index
+  var bucket = this._storage.get(index);
+  // if the 'bucket' array exists...
+  if (bucket) {
+    // Iterate through elements in 'bucket'
+    for (let i = 0; i < bucket.length; i++) {
+      var item = bucket[i];
+      // if array item key matches...
+      if (item['key'] === k) {
+        // return item value
+        return item['value'];
+      }
     }
-    // iterate through linked list
-    tuple = tuple['next'];
   }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  var tuple = this._storage.get(index);
-
-  if (tuple) {
-    // tuple head is what we're removing
-    if (tuple['key'] === k) {
-      // set linked list head to next tuple
-      this._storage.set(index, tuple['next']);
-    } else {
-      // search through linked list for next tuple with key
-      while (tuple) {
-        if (tuple['next'] && tuple['next']['key'] === k) {
-          // remove tuple from linked list
-          tuple['next'] = tuple['next']['next'];
-          return;
-        } else {
-          // iterate through linked list
-          tuple = tuple['next'];
-        }
+  // Get the 'bucket' array from storage at index
+  var bucket = this._storage.get(index);
+  // if the 'bucket' array exists...
+  if (bucket) {
+    // Iterate through elements in 'bucket'
+    for (let i = 0; i < bucket.length; i++) {
+      var item = bucket[i];
+      // if array item key matches...
+      if (item['key'] === k) {
+        // slice item out of bucket
+        bucket.splice(i, 1);
       }
     }
   }
